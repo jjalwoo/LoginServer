b2eed092dd9b51ff54f945752a4d92f3b6df57ba
jjalwoo <162006300+jjalwoo@users.noreply.github.com>
2024-05-01 17:31:55 +0900

    3. Redis 연동
    Redis db 연동 추가

---
+++ Redis/Redis.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace LoginServer.Redis
+{
+    public class Redis
+    {
+        ConnectionMultiplexer _redisConn;
+        IDatabase _redisDb;
+
+        public Redis()
+        {
+            _redisConn = ConnectionMultiplexer.Connect("localhost");
+            _redisDb = _redisConn.GetDatabase();
+        }
+
+        public void SetKeyValue(string key, string value)
+        {
+            _redisDb.StringSet(key, value);
+        }
+
+        public string GetValue(string key)
+        {
+            return _redisDb.StringGet(key);
+        }
+    }
+}
