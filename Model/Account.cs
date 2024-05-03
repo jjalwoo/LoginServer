0503165e5a8e8a31718a4897d5edcd78a14034e6
jjalwoo <162006300+jjalwoo@users.noreply.github.com>
2024-05-03 16:15:07 +0900

    LoginServer_240503
    DB 모델 추가

---
+++ Model/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml;
+
+namespace LoginServer.Model
+{
+    public class Account
+    {
+        public int Uid { get; set; }
+        public string? UserId { get; set; } = default;
+        public string? UserPw { get; set; } = default;
+        public DateTime CreateTime {  get; set; }
+        public DateTime DeleteTime { get; set; }
+        public DateTime LastLoginTime { get; set; }      
+    }
+}
